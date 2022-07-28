67e72f097731df625ea3059364a43731da22d626
Niklas Johansson <niklas@sluh.se>
2022-07-28 10:03:52 +0200

    Setup of prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,44 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'next/core-web-vitals',
-    'airbnb' /* 'airbnb-typescript', */,
-    'plugin:storybook/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {
-    'react/function-component-definition': [
-      1,
-      {
-        namedComponents: 'arrow-function',
-        unnamedComponents: 'arrow-function',
-      },
+    extends: [
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'next/core-web-vitals',
+        'airbnb' /* 'airbnb-typescript', */,
+        'plugin:storybook/recommended',
+        'prettier',
     ],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
-  },
-};
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'react/function-component-definition': [
+            1,
+            {
+                namedComponents: 'arrow-function',
+                unnamedComponents: 'arrow-function',
+            },
+        ],
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+        'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+    },
+}
