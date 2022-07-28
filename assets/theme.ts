c74097b06205e596abb5df345c3c352b8ce957d6
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2022-07-28 15:01:55 +0300

    Add primary colors

--- assets/theme.ts
+++ assets/theme.ts
@@ -1,5 +1,29 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react'
 
-const colors = {};
+const breakpoints = {
+  sm: '576px',
+  md: '1024px',
+  lg: '1440px',
+  xl: '1920px',
+}
 
-export default extendTheme({ colors });
+const colors = {
+  common: {
+    white: '#FFFFFF',
+    black: '#000000',
+  },
+  primary: {
+    brandingBlue: '#1172BA',
+    richBlue: '#0C2953',
+    darkBlue: '#0A2244',
+    richBlack: '#040404',
+    grey70: '#858585',
+    grey25: '#BFBFBF',
+    grey10: '#EBEBEB',
+    grey2: '#FAFAFA',
+    red: '#C4362D',
+    green: '#4AA233',
+  },
+}
+
+export default extendTheme({ breakpoints, colors })
