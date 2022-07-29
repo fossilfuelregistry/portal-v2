68e8dbe586d31b3750c9f0af4716911943d98f73
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2022-07-29 20:02:46 +0300

    Add map circles

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -3,13 +3,14 @@ import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import { Box } from '@chakra-ui/react'
 import mapStyle from './style.json'
+import data from './data.json'
 
 const Map = () => {
   const mapContainer = useRef<HTMLDivElement | null>(null)
   const map = useRef<any>(null)
   const [lng] = useState(47.19497528338846)
   const [lat] = useState(9.669634635566009)
-  const [zoom] = useState(1.7)
+  const [zoom] = useState(0)
 
   useEffect(() => {
     if (map.current || !mapContainer.current) return
@@ -23,7 +24,82 @@ const Map = () => {
       minZoom: 1.5,
       zoom,
     })
+
+    map.current.on('load', () => {
+      map.current.addSource('emissions', {
+        type: 'geojson',
+        data,
+      })
+      map.current.addLayer({
+        id: 'emissions-circles',
+        type: 'circle',
+        source: 'emissions',
+        paint: {
+          'circle-color': '#fff',
+          'circle-stroke-width': 1,
+          'circle-stroke-color': '#fff',
+          'circle-opacity': 0.4,
+          'circle-radius': [
+            'match',
+            ['get', 'country'],
+            'Ukraine',
+            10,
+            'Canada',
+            40,
+            'Brazil',
+            40,
+            'Australia',
+            40,
+            'Saudi Arabia',
+            40,
+            'India',
+            40,
+            'Korea',
+            15,
+            'Japan',
+            15,
+            'Guinea',
+            17,
+            'USA',
+            110,
+            'russ',
+            120,
+            'China',
+            130,
+            'Kazakhstan',
+            22,
+            'South Africa',
+            16,
+            'Angola',
+            16,
+            'Ghana',
+            11,
+            'Cameroon',
+            22,
+            'Niger',
+            22,
+            'Algeria',
+            25,
+            'Egypt',
+            18,
+            'Libya',
+            17,
+            'Turkey',
+            13,
+            'Italy',
+            10,
+            'Hungary',
+            10,
+            'Poland',
+            13,
+            /* other */ 10,
+          ],
+        },
+      })
+    })
   })
+
+  console.log('map.current')
 
   return (
     <Box w="100%" h="640px" p="relative" bg="#0A2244">
