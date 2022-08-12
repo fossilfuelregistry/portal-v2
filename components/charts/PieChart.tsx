08f8a45b3a478057737b66202200e18c76a4512d
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2022-08-12 21:24:14 +0300

    Add tooltip for Pie chart

--- components/charts/PieChart.tsx
+++ components/charts/PieChart.tsx
@@ -1,67 +1,16 @@
 import React, { FC } from 'react'
+import { Box, Flex } from '@chakra-ui/react'
 import { Pie } from '@visx/shape'
 import { Group } from '@visx/group'
+import { Tooltip, useTooltip } from '@visx/tooltip'
 import { colors } from '../../assets/theme'
-
-const data = [
-  {
-    fillColor: '#87BFFF',
-    fuel: 'oil',
-    label: 'Oil, combustion',
-    percentage: 10,
-    quantity: 80.89075427268527,
-    year: 2020,
-  },
-  {
-    fillColor: 'rgba(135, 191, 255, .5)',
-    fuel: 'oil',
-    label: 'Oil, pre-combustion',
-    percentage: 10,
-    quantity: 18.970718369092925,
-    subtype: undefined,
-    year: 2020,
-  },
-  {
-    fillColor: '#4C6EE6',
-    fuel: 'gas',
-    label: 'Gas, combustion',
-    percentage: 30,
-    quantity: 80.69994536364122,
-    subtype: undefined,
-    year: 2020,
-  },
-  {
-    fillColor: 'rgba(76, 110, 230, .5)',
-    fuel: 'gas',
-    label: 'Gas, pre-combustion',
-    percentage: 10,
-    quantity: 25.76536098012795,
-    subtype: undefined,
-    year: 2020,
-  },
-  {
-    fillColor: '#52B9BF',
-    fuel: 'coal',
-    label: 'Coal, combustion',
-    percentage: 30,
-    quantity: 0.03761302232599879,
-    year: 2020,
-  },
-  {
-    fillColor: 'rgba(82, 185, 191, .5)',
-    fuel: 'coal',
-    label: 'Coal, pre-combustion',
-    percentage: 10,
-    quantity: 0.0034353627768523866,
-    year: 2020,
-  },
-]
 
 type PieChartProps = {
   parentWidth: number
   parentHeight: number
   header: string
   value: string
+  data: any[]
 }
 
 const PieChart: FC<PieChartProps> = ({
@@ -69,60 +18,138 @@ const PieChart: FC<PieChartProps> = ({
   parentHeight,
   header,
   value,
+  data,
 }) => {
+  const {
+    tooltipOpen,
+    tooltipLeft,
+    tooltipTop,
+    tooltipData,
+    hideTooltip,
+    showTooltip,
+  } = useTooltip<any>()
+
   const minimumSize = Math.min(parentWidth, parentHeight)
   const radius = minimumSize / 2
+  const whiteSpace = 3
+  const innerLabel = 0.95
 
   return (
-    <svg width={parentWidth} height={parentHeight}>
-      <Group top={parentHeight / 2} left={parentWidth / 2}>
-        <Pie
-          data={data}
-          pieValue={(d) => d.percentage}
-          outerRadius={radius}
-          innerRadius={0.6 * radius}
-          cornerRadius={3}
-          padAngle={0.005}
-          pieSort={null}
-          pieSortValues={null}
+    <Box position="relative">
+      <svg width={parentWidth} height={parentHeight}>
+        <Group top={parentHeight / 2} left={parentWidth / 2}>
+          <Pie
+            data={data}
+            pieValue={(d) => d.percentage}
+            outerRadius={radius}
+            innerRadius={0.6 * radius}
+            cornerRadius={3}
+            padAngle={0.005}
+            pieSort={null}
+            pieSortValues={null}
+          >
+            {(pie) =>
+              pie.arcs.map((arc) => {
+                const arcPath = pie.path(arc) as string
+                const arcFill = arc.data.fillColor
+                const [centroidX, centroidY] = pie.path.centroid(arc)
+                const labelPosX =
+                  // eslint-disable-next-line no-nested-ternary
+                  centroidX > 0
+                    ? centroidX * innerLabel + whiteSpace
+                    : centroidX < 0
+                    ? centroidX * innerLabel - whiteSpace
+                    : centroidX
+                const labelPosY =
+                  // eslint-disable-next-line no-nested-ternary
+                  centroidY > 0
+                    ? centroidY * innerLabel + whiteSpace
+                    : centroidY < 0
+                    ? centroidY * innerLabel - whiteSpace
+                    : centroidY
+
+                return (
+                  <g key={`arc-${arc.data.label}`}>
+                    <path
+                      d={arcPath}
+                      fill={arcFill}
+                      onMouseLeave={() => {
+                        hideTooltip()
+                      }}
+                      onMouseEnter={() => {
+                        showTooltip({
+                          tooltipLeft: labelPosX + parentWidth / 2,
+                          tooltipTop: labelPosY - parentHeight / 2,
+                          tooltipData: arc.data,
+                        })
+                      }}
+                    />
+                  </g>
+                )
+              })
+            }
+          </Pie>
+        </Group>
+        <Group top={parentHeight / 2} left={parentWidth / 2}>
+          <text
+            x={0}
+            y={-10}
+            fill={colors.primary.richBlack}
+            fontSize={18}
+            fontFamily="Roboto"
+            textAnchor="middle"
+          >
+            {header}
+          </text>
+          <text
+            x={0}
+            y={25}
+            fill={colors.primary.richBlack}
+            fontSize={32}
+            fontFamily="sommet-rounded"
+            fontWeight="bold"
+            textAnchor="middle"
+          >
+            {value}
+          </text>
+        </Group>
+      </svg>
+      {tooltipData && tooltipOpen && (
+        <Tooltip
+          style={{
+            position: 'absolute',
+            transition: 'all 0.2s ease',
+            transform: `translate(calc(${tooltipLeft}px - 50%), calc(${tooltipTop}px - 50%))`,
+            backgroundColor: colors.primary.grey10,
+            borderRadius: 0,
+            paddingTop: 20,
+            paddingBottom: 20,
+            paddingLeft: 16,
+            paddingRight: 16,
+            color: colors.primary.richBlack,
+            fontSize: '14px',
+            fontWeight: 'normal',
+            display: 'inline-block',
+            pointerEvents: 'none',
+          }}
         >
-          {(pie) =>
-            pie.arcs.map((arc) => {
-              const arcPath = pie.path(arc) as string
-              const arcFill = arc.data.fillColor
-              return (
-                <g key={`arc-${arc.data.label}`}>
-                  <path d={arcPath} fill={arcFill} />
-                </g>
-              )
-            })
-          }
-        </Pie>
-      </Group>
-      <Group top={parentHeight / 2} left={parentWidth / 2}>
-        <text
-          x={0}
-          y={-10}
-          fill={colors.primary.richBlack}
-          fontSize={18}
-          fontFamily="Roboto"
-          textAnchor="middle"
-        >
-          {header}
-        </text>
-        <text
-          x={0}
-          y={25}
-          fill={colors.primary.richBlack}
-          fontSize={32}
-          fontFamily="sommet-rounded"
-          fontWeight="bold"
-          textAnchor="middle"
-        >
-          {value}
-        </text>
-      </Group>
-    </svg>
+          <Flex alignItems="center">
+            <Box
+              w="12px"
+              h="12px"
+              backgroundColor={tooltipData.fillColor}
+              borderRadius="100%"
+              mr="8px"
+            />
+            {tooltipData.label}:
+            <strong style={{ marginLeft: '4px' }}>
+              {' '}
+              {tooltipData.quantity} Mt CO₂e
+            </strong>
+          </Flex>
+        </Tooltip>
+      )}
+    </Box>
   )
 }
 
