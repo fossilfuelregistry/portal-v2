4202a4ef80b8276251d68efce7d83613a0f7e777
Niklas Johansson <niklas@sluh.se>
2022-09-15 21:11:04 +0200

    Fix bug with range of certainty in AnnualEmissions
    Re #163

--- hooks/useRangeOfCertainty.tsx
+++ hooks/useRangeOfCertainty.tsx
@@ -1,21 +1,18 @@
+import { EmissionsData } from 'lib/types'
 import { useMemo } from 'react'
 
 const useRangeOfCertainty = (
-  emissionsData: any,
+  emissionsData: EmissionsData,
   productionSourceId: number
 ) => {
   const rangeData = useMemo(() => {
-    const source = emissionsData.find(
-      (d: any) => d.sourceId === productionSourceId
-    )
+    const source = emissionsData?.find(d => d.sourceId === productionSourceId)
 
     if (source) {
-      return source.production.reduce((prev: any, curr: any) => {
-        const { p5: p5S1, wa: waS1, p95: p95S1 } = curr.co2e.scope1.total
-        const { p5: p5S3, wa: waS3, p95: p95S3 } = curr.co2e.scope3.total
-        const totalP5 = p5S1 + p5S3
-        const totalWa = waS1 + waS3
-        const totalP95 = p95S1 + p95S3
+      return source.production.reduce((prev:any, curr) => {
+        const { p5: p5S1, wa: waS1, p95: p95S1 } = curr.co2e!.scope1.total
+        const { p5: p5S3, wa: waS3, p95: p95S3 } = curr.co2e!.scope3.total
+        const { p5: totalP5, wa: totalWa, p95: totalP95 } = curr.co2e!.total.total
 
         if (!prev.length) {
           return [
@@ -37,17 +34,17 @@ const useRangeOfCertainty = (
         return [
           {
             value: [
-              p5S1 + prev[1].value[0],
-              waS1 + prev[1].value[1],
-              p95S1 + prev[1].value[2],
+              p5S1 + prev[0].value[0],
+              waS1 + prev[0].value[1],
+              p95S1 + prev[0].value[2],
             ],
             label: 'Pre-combustion',
           },
           {
             value: [
-              p5S3 + prev[0].value[0],
-              waS3 + prev[0].value[1],
-              p95S3 + prev[0].value[2],
+              p5S3 + prev[1].value[0],
+              waS3 + prev[1].value[1],
+              p95S3 + prev[1].value[2],
             ],
             label: 'Combustion',
           },
