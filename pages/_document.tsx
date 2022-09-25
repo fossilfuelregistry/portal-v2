ad9bb16115935d3735efca6ce7d6d8ec7d236e05
jesperwe <jesper@journeyman.se>
2022-09-25 09:57:01 +0200

    Mobile responsive improvements

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ import Script from 'next/script'
 const MyDocument = () => (
 	<Html>
 	<Head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<link rel="preconnect" href="https://fonts.googleapis.com"/>
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
 		<link
